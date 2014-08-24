183e519f040a41ab45b4b23a62d37982a1c9f9e6
dengn <nicolasdeng1989@gmail.com>
2014-08-24 21:47:27 +0200

    Improve the Choose Plate Page

--- src/com/melbournestore/activities/PlateActivity.java
+++ src/com/melbournestore/activities/PlateActivity.java
@@ -22,11 +22,15 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.NavUtils;
 import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.melbournestore.adaptors.PlateListAdapter;
 
@@ -36,6 +40,10 @@ public class PlateActivity extends Activity {
 	
     private ListView mPlatesList;
     
+    private Button mConfirmChoice;
+    
+    private TextView mTotalPrice;
+    
     private int[] plateImages = {R.drawable.plate1, R.drawable.plate2, R.drawable.plate3, R.drawable.plate4, R.drawable.plate5};
 
     private String[] plateTitles = {"麻辣小龙虾",
@@ -44,6 +52,35 @@ public class PlateActivity extends Activity {
         "咖喱小龙虾",
         "小龙虾炒年糕"};
 
+    private int[] platePrices = {55, 55, 58, 58, 55};
+    private String[] plateStocks = {"今日库存20份","今日库存20份","今日库存20份","今日库存10kg","今日库存10份"};
+    
+    private String[] plateNumbers = {"1","1","2","1","2"};
+    
+    private int totalPrice = 0; 
+    
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle b = msg.getData();
+            int position = b.getInt("position");
+            switch(msg.what) {
+            	//plus = 1 
+                case 1:
+                	totalPrice+=platePrices[position];
+                	mTotalPrice.setText("$"+String.valueOf(totalPrice));
+                    break;
+                //minus = 2
+                case 2:
+                	totalPrice-=platePrices[position];
+                	mTotalPrice.setText("$"+String.valueOf(totalPrice));
+                    break;
+
+            }
+        }
+         
+    };
+    
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.plate_layout);
@@ -55,8 +92,23 @@ public class PlateActivity extends Activity {
         // button will take the user one step up in the application's hierarchy.
         actionBar.setDisplayHomeAsUpEnabled(true);
         
+
+        
         mPlatesList = (ListView) findViewById(R.id.plates_list);
-        mPlatesList.setAdapter(new PlateListAdapter(this, plateTitles, plateImages));
+        
+        String[] platePrices_string = new String[platePrices.length];
+        for(int i=0;i<platePrices.length;i++){
+        	platePrices_string[i]="$"+String.valueOf(platePrices[i]);
+        }
+        
+        mPlatesList.setAdapter(new PlateListAdapter(this, mHandler, plateTitles, platePrices_string, plateStocks, plateNumbers, plateImages));
+
+        
+        mConfirmChoice = (Button) findViewById(R.id.confirm_choice);
+        mConfirmChoice.bringToFront();
+        
+        mTotalPrice = (TextView) findViewById(R.id.confirm_price);
+        mTotalPrice.setText("$"+String.valueOf(totalPrice));
         
         Intent intent = getIntent();
         int category = intent.getIntExtra("Category",0);
@@ -92,5 +144,15 @@ public class PlateActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    
+//    /* The click listner for ListView in the navigation drawer */
+//    private class PlateItemClickListener implements ListView.OnItemClickListener {
+//        @Override
+//        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//            //selectItem(position);
+//        	Log.d(TAG, String.valueOf(position)+" plat item clicked");
+//        }
+//    }
+//    
     
 }
--- src/com/melbournestore/adaptors/PlateListAdapter.java
+++ src/com/melbournestore/adaptors/PlateListAdapter.java
@@ -1,29 +1,46 @@
 package com.melbournestore.adaptors;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.melbournestore.activities.R;
 
 public class PlateListAdapter extends BaseAdapter {
-	String[] result;
+	String[] plate_names;
+	String[] plate_prices;
+	String[] plate_stocks;
+	
+	String[] plates_nums;
+
 	Context mContext;
 	int[] imageId;
+
+	Handler mHandler;
+
 	private static LayoutInflater inflater = null;
 
-	public PlateListAdapter(Context context, String[] prgmNameList,
-			int[] prgmImages) {
+	public PlateListAdapter(Context context, Handler handler, String[] names,
+			String[] prices, String[] stocks, String[] numbers, int[] imgs) {
 		// TODO Auto-generated constructor stub
-		result = prgmNameList;
+		plate_names = names;
+		plate_prices = prices;
+		plate_stocks = stocks;
+		plates_nums = numbers;
+		
 		mContext = context;
-		imageId = prgmImages;
+		mHandler = handler;
+
+		imageId = imgs;
 		inflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	}
@@ -31,7 +48,7 @@ public class PlateListAdapter extends BaseAdapter {
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
-		return result.length;
+		return plate_names.length;
 	}
 
 	@Override
@@ -47,8 +64,15 @@ public class PlateListAdapter extends BaseAdapter {
 	}
 
 	public class Holder {
-		TextView tv;
-		ImageView img;
+		TextView names_view;
+		TextView prices_view;
+		TextView stocks_view;
+		ImageView imgs_view;
+		
+		TextView num_view;
+
+		Button plus;
+		Button minus;
 	}
 
 	@Override
@@ -57,18 +81,67 @@ public class PlateListAdapter extends BaseAdapter {
 		Holder holder = new Holder();
 		View rowView;
 		rowView = inflater.inflate(R.layout.plate_list_item, null);
-		holder.tv = (TextView) rowView.findViewById(R.id.plate_txt);
-		holder.img = (ImageView) rowView.findViewById(R.id.plate_img);
-		holder.tv.setText(result[position]);
-		holder.img.setImageResource(imageId[position]);
-		rowView.setOnClickListener(new OnClickListener() {
+		holder.names_view = (TextView) rowView.findViewById(R.id.plate_name);
+		holder.prices_view = (TextView) rowView.findViewById(R.id.plate_price);
+		holder.stocks_view = (TextView) rowView.findViewById(R.id.plate_like);
+		holder.imgs_view = (ImageView) rowView.findViewById(R.id.plate_img);
+		holder.num_view = (TextView) rowView.findViewById(R.id.plate_number);
+
+		holder.plus = (Button) rowView.findViewById(R.id.plate_plus);
+		holder.minus = (Button) rowView.findViewById(R.id.plate_minus);
+
+		holder.names_view.setText(plate_names[position]);
+		holder.prices_view.setText(plate_prices[position]);
+		holder.stocks_view.setText(plate_stocks[position]);
+		holder.imgs_view.setImageResource(imageId[position]);
+		
+		holder.num_view.setText(plates_nums[position]);
+		// rowView.setOnClickListener(new OnClickListener() {
+		// @Override
+		// public void onClick(View v) {
+		// // TODO Auto-generated method stub
+		// Toast.makeText(mContext, "You Clicked " + plate_names[position],
+		// Toast.LENGTH_LONG).show();
+		//
+		//
+		// }
+		// });
+
+		holder.plus.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Toast.makeText(mContext, "You Clicked " + result[position],
-						Toast.LENGTH_LONG).show();
+				Message message = new Message();
+				Bundle b = new Bundle();
+				// send the position
+				b.putInt("position", position);
+				message.setData(b);
+
+				// plus = 1
+				message.what = 1;
+
+				mHandler.sendMessage(message);
 			}
 		});
+
+		holder.minus.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Message message = new Message();
+				Bundle b = new Bundle();
+				// send the position
+				b.putInt("position", position);
+				message.setData(b);
+
+				// minus = 2
+				message.what = 2;
+
+				mHandler.sendMessage(message);
+
+			}
+		});
+
 		return rowView;
 	}
 
