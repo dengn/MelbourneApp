d1ed62e2696ae07dec5631339cca71a929221d93
dengn <nicolasdeng1989@gmail.com>
2014-09-05 10:36:07 +0200

    Finish adding all data models

--- src/com/melbournestore/models/Order.java
+++ src/com/melbournestore/models/Order.java
@@ -41,6 +41,7 @@ public class Order {
 		this.update_time = update_time;
 		this.confirm_time = confirm_time;
 		this.distributing_time = distributing_time;
+		this.deliverying_time = deliverying_time;
 		this.complete_time = complete_time;
 		this.status = status;
 		this.items = items;
@@ -62,12 +63,42 @@ public class Order {
 	public String getPostCode(){
 		return post_code;
 	}
-	
-	public void getDeliveryTime(){
-		this.delivery_time = delivery_time;
+	public String getDeliveryTime(){
+		return delivery_time;
 	}
-	
-	
+	public int getDeliveryFee(){
+		return delivery_fee;
+	}
+	public String getRemark(){
+		return remark;
+	}
+	public String getCreateTime(){
+		return create_time;
+	}
+	public String getUpdateTime(){
+		return update_time;
+	}
+	public String getConfirmTime(){
+		return confirm_time;
+	}
+	public String getDistributingTime(){
+		return distributing_time;
+	}
+	public String getDeliveryingTime(){
+		return deliverying_time;
+	}
+	public String getCompleteTime(){
+		return complete_time;
+	}
+	public int getStatus(){
+		return status;
+	}
+	public OrderItem getItems(){
+		return items;
+	}
+	public Suburb getSuburb(){
+		return suburb;
+	}
 	
 	
 	public void setId(int id){
@@ -78,6 +109,48 @@ public class Order {
 	}
 	public void setUnitNo(String unit_no){
 		this.unit_no = unit_no;
+	}
+	public void setStreet(String street){
+		this.street = street;
+	}
+	public void setPostCode(String post_code){
+		this.post_code = post_code;
+	}
+	public void setDeliveryTime(String delivery_time){
+		this.delivery_time = delivery_time;
+	}
+	public void setDeliveryFee(int delivery_fee){
+		this.delivery_fee = delivery_fee;
+	}
+	public void setRemark(String remark){
+		this.remark = remark;
+	}
+	public void setCreateTime(String create_time){
+		this.create_time = create_time;
+	}
+	public void setUpdateTime(String update_time){
+		this.update_time = update_time;
+	}
+	public void setConfirmTime(String confirm_time){
+		this.confirm_time = confirm_time;
+	}
+	public void setDistributingTime(String distributing_time){
+		this.distributing_time = distributing_time;
+	}
+	public void setDeliveryingTime(String deliverying_time){
+		this.deliverying_time = deliverying_time;
+	}
+	public void setCompleteTime(String complete_time){
+		this.complete_time = complete_time;
+	}
+	public void setStatus(int status){
+		this.status = status;
+	}
+	public void setItems(OrderItem items){
+		this.items = items;
+	}
+	public void setSuburb(Suburb suburb){
+		this.suburb = suburb;
 	}
 
 }
